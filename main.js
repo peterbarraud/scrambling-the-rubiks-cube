f4ab2f280ede06c7a14aa4f18fd5ef5a763ec246
Peter G.A. Barraud <barraud@adobe.com>
2020-12-10 22:06:03 +0530

    implementing img insert

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     var scrabmle_str = "U2 R2 U R' F' R2 D B2 U L' D U2 F2 U2 R' D2 B' F2 L2 B2 U B2 L2 R B' F' L B F U".toLowerCase();
+    $ ( '#scramble' ).text("U2 R2 U R' F' R2 D B2 U L' D U2 F2 U2 R' D2 B' F2 L2 B2 U B2 L2 R B' F' L B F U");
     $( scrabmle_str.split(' ') ).each(function(i, move) {
         var moves = [];
         if (move[1] === "'"){
