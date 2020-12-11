2ade2246b02d69ec76446b8c0ca313523f544b11
Peter G.A. Barraud <barraud@adobe.com>
2020-12-11 19:56:39 +0530

    fitting to screen

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
-    const row_len = 7;
+    var screenwidth = parseInt($(window).width());
+    var imagewidth = screenwidth < 700 ? 70 : 120;
+    $( '#versionnumber' ).text($('meta[name=versionnumber]').attr('content'));
+    $( '#screenwidth' ).text(screenwidth);
+    const row_len = screenwidth < 700 ? 4 : 12;
     var scrabmle_str = "L2 R D2 L' B F' L D' U2 F L' R F D2 U B' L' R2 B' R D L' D' L2 B' R U' R' B2 D";
     $ ( '#scramble' ).text(scrabmle_str);
     var moves = [];
@@ -12,11 +16,10 @@ $(document).ready(function(){
                 moves.push(move[0]);
         }        
       });
-      var cell_count = 0;
       for (var i=0;i<moves.length;i++){
           var tr = $( '<tr/>' );
           for (var j=0;j<row_len;j++){
-              tr.append('<td><img src="imgs/' + moves[i] + '.png" width=70px></td>');
+              tr.append('<td><img src="imgs/' + moves[i] + '.png" width=' + imagewidth + 'px></td>');
               i += 1;
               if (i==moves.length){
                   break;
