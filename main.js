a50eca42f574843895405f096940f8f688066dc4
Peter G.A. Barraud <barraud@adobe.com>
2020-12-11 15:07:14 +0530

    not including dynamic scramble

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
 $(document).ready(function(){
-    var scrabmle_str = "U2 R2 U R' F' R2 D B2 U L' D U2 F2 U2 R' D2 B' F2 L2 B2 U B2 L2 R B' F' L B F U".toLowerCase();
-    $ ( '#scramble' ).text("U2 R2 U R' F' R2 D B2 U L' D U2 F2 U2 R' D2 B' F2 L2 B2 U B2 L2 R B' F' L B F U");
-    $( scrabmle_str.split(' ') ).each(function(i, move) {
-        var moves = [];
+    const row_len = 7;
+    var scrabmle_str = "L2 R D2 L' B F' L D' U2 F L' R F D2 U B' L' R2 B' R D L' D' L2 B' R U' R' B2 D";
+    $ ( '#scramble' ).text(scrabmle_str);
+    var moves = [];
+    $( scrabmle_str.toLocaleLowerCase().split(' ') ).each(function(i, move) {
         if (move[1] === "'"){
             moves.push(move[0] + '-prime');
         } else{
             if (move[1] === "2")
                 moves.push(move[0]);
                 moves.push(move[0]);
-        }
-        $ (moves).each(function(i, move){
-            $( "table" ).append( "<tr><td><img src='imgs/" + move + ".png'></td?</tr>" );
-            console.log(move)
-        });
+        }        
       });
+      var cell_count = 0;
+      for (var i=0;i<moves.length;i++){
+          var tr = $( '<tr/>' );
+          for (var j=0;j<row_len;j++){
+              tr.append('<td><img src="imgs/' + moves[i] + '.png"></td>');
+              i += 1;
+              if (i==moves.length){
+                  break;
+              }
+          }
+          $( 'table' ).append(tr);
+      }
+
+      console.log('all don')
   });
